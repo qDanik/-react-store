31f0eb64b67c9d634c71bcd53d9a5d88bbffa041
Daniil <daniil.yankouski@kronos.com>
2020-09-25 00:03:05 +0300

    Off Release: 2.0.0 (2.0.1) skip update when values is equal(shadow equal)

--- src/core/handlers/set.ts
+++ src/core/handlers/set.ts
@@ -2,8 +2,14 @@ import {ObserverStore} from '../watchStore';
 
 export function set<Target extends ObserverStore>
 (this: any, target: Target, property: string, value: any): boolean {
+  const isEqual: boolean = this[property] === value;
   this[property] = value;
-  if (property !== 'update' && !property.startsWith('_') && typeof this[property] !== 'function') {
+
+  if (property !== 'update'
+    && !isEqual
+    && !property.startsWith('_')
+    && typeof this[property] !== 'function'
+  ) {
     target.update();
   }
 
